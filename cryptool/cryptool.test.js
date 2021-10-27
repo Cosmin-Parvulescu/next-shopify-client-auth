f77830cce55f8f80273c7bc7500f046893a9b86b
Cosmin Parvulescu <cosmin.parvulescu@gmail.com>
2021-10-27 21:46:06 +0300

    Added some tests for the cryptool

---
+++ cryptool/cryptool.test.js
@@ -0,0 +1,37 @@
+
+require('@babel/register')({
+    presets: ['@babel/preset-env'],
+    ignore: ['node_modules']
+});
+
+import Cryptool from './cryptool'
+
+test('Decrypting encrypted string works', () => {
+    const cryptool = new Cryptool('ENCRYPTION KEY 🚀');
+
+    const plaintext = 'The cake is a lie';
+    const encrypted = cryptool.encrypt(plaintext);
+    const decrypted = cryptool.decrypt(encrypted);
+
+    expect(decrypted).toBe(plaintext);
+});
+
+test('Encrypting at least changes form', () => {
+    const cryptool = new Cryptool('ENCRYPTION KEY 🚀');
+
+    const plaintext = 'The cake is a lie';
+    const encrypted = cryptool.encrypt(plaintext);
+
+    expect(encrypted).not.toBe(plaintext);
+})
+
+test('Throws exception if IV is not present in key', () => {
+    const cryptool = new Cryptool('ENCRYPTION KEY 🚀');
+
+    const plaintext = 'The cake is a lie';
+    const encrypted = cryptool.encrypt(plaintext);
+
+    const noIV = encrypted.split('|')[0];
+
+    expect(() => cryptool.decrypt(noIV)).toThrow();
+})
