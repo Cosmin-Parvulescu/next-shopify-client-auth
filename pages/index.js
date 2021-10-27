0c7555beafd990455906cd37b298df61caae138a
Cosmin Parvulescu <cosmin.parvulescu@gmail.com>
2021-10-27 15:01:06 +0300

    Loaded Shopify REST API response into frontend

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
-import { Layout, Page } from "@shopify/polaris";
+import Cryptool from '../cryptool';
+import Shopify from '@shopify/shopify-api';
 
-export default function Index() {
+import { Layout, Page } from "@shopify/polaris";
+import CustomerOrders from '../components/customer-orders';
+
+export default function Index(props) {
   return (
     <Page
       title="App"
     >
       <Layout>
         <Layout.Section>
-          <h1>Hello?</h1>
+          {props.customers && 
+          props.customers.length > 0 && 
+          <CustomerOrders customers={props.customers}></CustomerOrders>}
         </Layout.Section>
       </Layout>
     </Page>
@@ -25,10 +31,27 @@ export async function getServerSideProps(ctx) {
     }
   }
 
+  const shopData = JSON.parse(Cryptool.dencrypt(shopCookie));
+
+  const client = new Shopify.Clients.Rest(shopData.shop, shopData.token);
+  const customerQryRes = await client.get({
+    path: 'customers/search',
+    query: {
+      "order": "orders_count DESC",
+      "limit": 10
+    }
+  })
+
+  const mappedCustomers = customerQryRes.body.customers.map(c => {
+    return {
+      name: `${c.first_name} ${c.last_name}`,
+      orders_count: c.orders_count
+    }
+  })
+
   return {
     props: {
-
+      customers: mappedCustomers
     }
   }
 }
-
